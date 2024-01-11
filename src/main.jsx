e9664083ac9ab29cec20c490a343e63e5cd51492
Beka Chogovadze <chogovadzebeka@gmail.com>
2024-01-11 23:37:25 +0400

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import Header from "./assets/components/header/header.jsx";
 import Cards from "./assets/components/cards/cards.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
     <Header />
     <Cards />
   </React.StrictMode>
